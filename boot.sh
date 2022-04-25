c5934b2c9f1c3d638f46f046ae835374945b0bb6
AlexandreGazagnes <a_syoez@yahoo.fr>
2022-04-25 15:44:56 +0200

    update boot.sh

--- boot.sh
+++ boot.sh
@@ -3,9 +3,12 @@ sudo apt install python-pip
 sudo apt install python3-dev
 sudo apt install python3.10-venv
 
-
 python3 -m venv env
 source env/bin/activate
 
+git pull
 
 pip install -r requirements.txt
+
+chmod +x app.py
+python3 app.py
