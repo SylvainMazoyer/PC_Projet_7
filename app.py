4d8e49a7f533cba58468b7418a7091d90b82238d
AlexandreGazagnes <a_syoez@yahoo.fr>
2022-04-26 09:27:22 +0200

    uncomment lines

--- app.py
+++ app.py
@@ -47,37 +47,58 @@ list_columns = df.columns.tolist()
 list_columns.remove("SK_ID_CURR")
 list_columns.remove("TARGET")
 select_features = list_columns
-sku = 0
+# sku = 0
 
 
 # route test
 @app.route("/hello", methods=["GET"])
 def hello():
+
     return ("<h1> Welcome </h1>", 200)
 
 
-# @app.route("/predict", methods=["GET", "POST"])
-# def predict():
+# send client id
+@app.route("/send_sku", methods=["GET"])
+def send_sku():
 
-#     json_sku = json.loads(request.data)
-#     sku = int(json_sku["sku"])
-#     print("sku :" + str(sku))
-#     X_sku = df.loc[(df["SK_ID_CURR"] == sku), select_features]
-#     result_pred = estimator.predict(X_sku)[0]
-#     result_pred_proba = estimator.predict_proba(X_sku)[0]
-#     print("result pred :" + str(result_pred))
-#     if result_pred == 0:
-#         result_reimbursement = "Ok"
-#     else:
-#         result_reimbursement = "Not ok"
-#     # Utiliser module Loging, log écrit dans un fichier log
+    serie_sku = df.iloc[0:50, 0]
+    dict_sku = serie_sku.to_dict()
+    json_dict_sku = jsonify(dict_sku)
 
-#     print("Reimbursement : " + result_reimbursement)
+    return json_dict_sku
 
-#     pred_dict = {"pred": int(result_pred), "proba_0": result_pred_proba[0]}
-#     json_pred = json.dumps(pred_dict)
-#     print(pred_dict)
-#     return json_pred
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    # recupere id du client
+    json_sku = json.loads(request.data)  # remplacé par request.json()
+    sku = int(json_sku["sku"])
+    print("sku :" + str(sku))
+
+    # extract features for the sku
+    X_sku = df.loc[(df["SK_ID_CURR"] == sku), select_features]
+
+    # # make preds
+    # result_pred = estimator.predict(X_sku)[0]
+    # result_pred_proba = estimator.predict_proba(X_sku)[0]
+    # print("result pred :" + str(result_pred))
+    # if result_pred == 0:
+    #     result_reimbursement = "Ok"
+    # else:
+    #     result_reimbursement = "Not ok"
+
+    # # Utiliser module Loging, log écrit dans un fichier log
+    # print("Reimbursement : " + result_reimbursement)
+
+    # # build json
+    # pred_dict = {"pred": int(result_pred), "proba_0": result_pred_proba[0]}
+    # json_pred = json.dumps(pred_dict)
+    # print(pred_dict)
+
+    # return json_pred
+
+    return X_sku.to_json()
 
 
 # # send feature importance
@@ -99,14 +120,5 @@ def hello():
 #     return json_shap_data
 
 
-# # send client id
-# @app.route("/send_sku", methods=["GET"])
-# def send_sku():
-#     serie_sku = df.iloc[0:50, 0]
-#     dict_sku = serie_sku.to_dict()
-#     json_dict_sku = jsonify(dict_sku)
-#     return json_dict_sku
-
-
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
